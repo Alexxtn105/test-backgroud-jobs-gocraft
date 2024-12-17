04afdcc8136854fda0125f651e2f2f1bc9d03dae
Alexxtn105 <netmann105@gmail.com>
2024-12-17 12:27:32 +0300

    Простейший рабочий вариант

---
+++ enqueuer/enqueuer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gocraft/work"
+	"github.com/gomodule/redigo/redis"
+	"log"
+)
+
+// redisPool Пул очередей фоновых задач с использованием redis
+var redisPool = &redis.Pool{
+	MaxActive: 5, // максимум активных задач
+	MaxIdle:   5, // максимум простаивающих задач
+	Wait:      true,
+	Dial: func() (redis.Conn, error) {
+		return redis.Dial("tcp", "localhost:6379")
+	},
+}
+
+// enqueuer постановщик задач в очередь (пространство имен - demo_app)
+var enqueuer = work.NewEnqueuer("demo_app", redisPool)
+
+func main() {
+	_, err := enqueuer.Enqueue(
+		"email", // имя задачи
+		work.Q{"email": "test@mail.ru", "subject": "Testing!"}, //аргументы задачи
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- worker.go
+++ worker.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gocraft/work"
+	"github.com/gomodule/redigo/redis"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// redisPool Пул очередей фоновых задач с использованием redis. Используется для того, чтобы брать задачи из очереди
+var redisPool = &redis.Pool{
+	MaxActive: 5, // максимум активных задач
+	MaxIdle:   5, // максимум простаивающих задач
+	Wait:      true,
+	Dial: func() (redis.Conn, error) {
+		return redis.Dial("tcp", "localhost:6379")
+	},
+}
+
+// Context эта структура требуется для пакета gocraft/work
+type Context struct{}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// создаем пул воркеров, который может исполнять несколько задач одновременно
+	pool := work.NewWorkerPool(
+		Context{},
+		10,         // количество одновременно выполняемых задач
+		"demo_app", // пространство имен очереди
+		redisPool,  // пул задач redis
+	)
+
+	// создаем маппинг имен задачь к соответствующим им функциям выполнения
+	pool.Job("email", SendEmail)
+
+	// стартуем пул задач
+	pool.Start()
+
+	// ожидаем сигнала выхода (graceful stop)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	<-signalChan
+
+	// останавливаем пул
+	pool.Stop()
+}
+
+func SendEmail(job *work.Job) error {
+	addr := job.ArgString("email")
+	subject := job.ArgString("subject")
+	if err := job.ArgError(); err != nil {
+		return err
+	}
+
+	fmt.Println("Sneding mail to ", addr, "with subject", subject)
+	time.Sleep(2 * time.Second)
+	return nil
 }
